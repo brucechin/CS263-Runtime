d1c1c2fcda710d6c087b04ff5cdf4fecb3e84e68
hgyhungry <ninahuangguyue@hotmail.com>
2021-05-24 10:48:10 +0800

    add go roaring

---
+++ go/roaring/run_bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[Go][Benchmark] bitmap"
+
+
+go test -bench Benchmark -run -
+echo
+
+real=${1:-"noreal"}
+
+if [ $real == "real" ] 
+then
+
+echo "[Go][Benchmark] bitmap real datasets"
+
+go get github.com/RoaringBitmap/real-roaring-datasets
+BENCH_REAL_DATA=1 go test -bench BenchmarkRealData -run -
+echo
+fi
