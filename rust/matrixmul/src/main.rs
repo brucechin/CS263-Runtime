9632c5572888a52bad9e0662a596bead33658721
Lianke Qin <lianke.qin@gmail.com>
2021-05-25 23:06:28 -0500

    Update main.rs

--- rust/matrixmul/src/main.rs
+++ rust/matrixmul/src/main.rs
@@ -1,13 +1,12 @@
-
-type Matrix = Vec<Vec<f32>>;
+type Matrix = Vec<Vec<i32>>;
 use nalgebra::{ArrayStorage, Dynamic, Matrix as NMatrix, VecStorage};
 use nalgebra::{U100, U20};
 use ndarray::Array;
 use tch::{Device, Kind, Tensor};
 
 fn get_matrices(n: usize) -> (Matrix, Matrix) {
-    let a: Matrix = vec![vec![0.1; n]; n];
-    let b: Matrix = vec![vec![0.1; n]; n];
+    let a: Matrix = vec![vec![1i32; n]; n];
+    let b: Matrix = vec![vec![1i32; n]; n];
     (a, b)
 }
 fn matmul_torch(n: i64) {
@@ -25,14 +24,19 @@ fn matmul_ndarray(n: usize) {
 
 fn matmul(n: usize) {
     let (a, b) = get_matrices(n);
-    let mut out: Matrix = vec![vec![0.0; n]; n];
+    let mut out: Matrix = vec![vec![0; n]; n];
+    let time = std::time::Instant::now();
     for i in 0..n {
         for j in 0..n {
+            let mut tmp = 0i32;
             for k in 0..n {
-                out[i][j] += a[i][k] * b[k][j];
+                tmp += a[i][k] * b[k][j];
             }
+            out[i][j] = tmp;
         }
     }
+    let took = time.elapsed();
+    println!("{} -- length {} -- {} seconds", "Naive", n, took.as_secs_f64());
 }
 
 
@@ -40,15 +44,13 @@ fn base_matmul() {
     let retries = 2;
     for n in [ 500, 1000, 2000, 4000, 8000].iter() {
         for j in 0..retries{
-            let time = std::time::Instant::now();
             matmul(*n as usize);
-            let took = time.elapsed();
-            println!("{} -- length {} -- {} seconds", "Naive", n, took.as_secs_f64());
+
     
-            let time = std::time::Instant::now();
-            matmul_torch(*n as i64);
-            let took = time.elapsed();
-            println!("{} -- length {} -- {} seconds", "Torch", n, took.as_secs_f64());
+            // let time = std::time::Instant::now();
+            // matmul_torch(*n as i64);
+            // let took = time.elapsed();
+            // println!("{} -- length {} -- {} seconds", "Torch", n, took.as_secs_f64());
         }
 
     }
