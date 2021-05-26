20566bbde857de4b2501c95dbfbeadabd0115779
Lianke Qin <lianke.qin@gmail.com>
2021-05-25 23:09:08 -0500

    Update main.rs

--- rust/matrixmul/src/main.rs
+++ rust/matrixmul/src/main.rs
@@ -1,49 +1,27 @@
-type Matrix = Vec<Vec<i32>>;
-use nalgebra::{ArrayStorage, Dynamic, Matrix as NMatrix, VecStorage};
-use nalgebra::{U100, U20};
-use ndarray::Array;
-use tch::{Device, Kind, Tensor};
-
-fn get_matrices(n: usize) -> (Matrix, Matrix) {
-    let a: Matrix = vec![vec![1i32; n]; n];
-    let b: Matrix = vec![vec![1i32; n]; n];
-    (a, b)
-}
-fn matmul_torch(n: i64) {
-    let a = Tensor::ones(&[n, n], (Kind::Float, Device::Cpu)) * 0.1;
-    let b = Tensor::ones(&[n, n], (Kind::Float, Device::Cpu)) * 0.1;
-    let o = a.matmul(&b);
-}
-
-
-fn matmul_ndarray(n: usize) {
-    let a = Array::<f32, _>::from_elem((n, n), 0.1);
-    let b = Array::<f32, _>::from_elem((n, n), 0.1);
-    let out = a.dot(&b);
-}
-
 fn matmul(n: usize) {
     let (a, b) = get_matrices(n);
     let mut out: Matrix = vec![vec![0; n]; n];
     let time = std::time::Instant::now();
-    for i in 0..n {
-        for j in 0..n {
-            let mut tmp = 0i32;
-            for k in 0..n {
-                tmp += a[i][k] * b[k][j];
+    let retries = 5;
+    for i in 0..retries {
+        for i in 0..n {
+            for j in 0..n {
+                let mut tmp = 0i32;
+                for k in 0..n {
+                    tmp += a[i][k] * b[k][j];
+                }
+                out[i][j] = tmp;
             }
-            out[i][j] = tmp;
         }
     }
     let took = time.elapsed();
-    println!("{} -- length {} -- {} seconds", "Naive", n, took.as_secs_f64());
+    println!("{} -- length {} -- {} seconds", "Naive", n, took.as_secs_f64() / retries);
 }
 
 
 fn base_matmul() {
-    let retries = 2;
+    
     for n in [ 500, 1000, 2000, 4000, 8000].iter() {
-        for j in 0..retries{
             matmul(*n as usize);
 
     
@@ -51,13 +29,8 @@ fn base_matmul() {
             // matmul_torch(*n as i64);
             // let took = time.elapsed();
             // println!("{} -- length {} -- {} seconds", "Torch", n, took.as_secs_f64());
-        }
+        
 
     }
 
-}
-
-
-fn main() {
-    base_matmul();
 }
